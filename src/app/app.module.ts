29cb7237386c3dce0c3d159cc2f83242beda927e
celesdv <66212987+celesdv@users.noreply.github.com>
2023-03-21 15:08:02 -0300

    feat: initial setup, create structure and login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { SpinnerComponent } from './shared/spinner/spinner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    DashboardComponent,
+    NavbarComponent,
+    SpinnerComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/interfaces/client.ts
@@ -0,0 +1,9 @@
+export interface Client {
+  id: number;
+  first_name: string;
+  last_name: string;
+  email: string;
+  address: string;
+  phone: string;
+  cuil: string;
+}
